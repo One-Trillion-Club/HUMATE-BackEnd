7948b384e0896130da22dabb70236ef6d745c740
Hypersand <on8214@gmail.com>
2024-07-29 15:00:11 +0900

    ✨ Feat: 활동 기록하기 기능 구현

--- src/main/java/com/otclub/humate/domain/activity/controller/ActivityController.java
+++ src/main/java/com/otclub/humate/domain/activity/controller/ActivityController.java
@@ -1,14 +1,18 @@
 package com.otclub.humate.domain.activity.controller;
 
-import com.otclub.humate.common.upload.S3Uploader;
+import com.otclub.humate.common.dto.CommonResponseDTO;
 import com.otclub.humate.domain.activity.dto.ActivitiesResponseDTO;
 import com.otclub.humate.domain.activity.dto.NewActivityResponseDTO;
+import com.otclub.humate.domain.activity.dto.UploadActivityRequestDTO;
 import com.otclub.humate.domain.activity.service.ActivityService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RequestMapping("/activities")
 @RestController
@@ -16,14 +20,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 public class ActivityController {
     private final ActivityService activityService;
-    private final S3Uploader s3Uploader;
 
     /**
      * 특정 동행의 활동(완료된, 새로운) 목록 조회
      *
-     * @author : 손승완
      * @param : 동행 ID
-     *
+     * @author : 손승완
      */
     @GetMapping
     public ResponseEntity<ActivitiesResponseDTO> activityList(@RequestParam("companionId") int companionId) {
@@ -32,8 +34,9 @@ public class ActivityController {
 
     /**
      * 새로운 활동 상세 조회
-     * @author : 손승완
+     *
      * @param : 활동 인증 내역 ID
+     * @author : 손승완
      */
     @GetMapping("/{activityId}")
     public ResponseEntity<NewActivityResponseDTO> NewActivityDetails(@PathVariable("activityId") int activityId) {
@@ -42,8 +45,9 @@ public class ActivityController {
 
     /**
      * 완료된 활동 상세 조회
-     * @author : 손승완
+     *
      * @param : 활동 인증 내역 ID
+     * @author : 손승완
      */
     @GetMapping("/histories/{companionActivityId}")
     public ResponseEntity<Object> finishedActivityDetails(@PathVariable("companionActivityId") int companionActivityId) {
@@ -52,22 +56,16 @@ public class ActivityController {
 
     /**
      * 활동 기록하기
+     *
+     * @param : 활동 ID, 동행 ID, 이미지 파일 목록
      * @author : 손승완
-     * @param : 활동 ID
-     * @param : 동행 ID
      */
-    @PostMapping("/upload")
-    public ResponseEntity<Object> uploadActivity(@RequestParam("activityId") int activityId,
-                                                 @RequestParam("companionId") int companionId,
-                                                 @RequestPart("image") MultipartFile imageFile) {
+    @PostMapping(value = "/upload", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<CommonResponseDTO> activityUpload(
+                                                            @RequestPart("uploadActivityRequestDTO") UploadActivityRequestDTO uploadActivityRequestDTO,
+                                                            @RequestPart("images") List<MultipartFile> images) {
 
-        // 해당 동행에 대해서 이미 올린 활동인지 먼저 검증해야한다.
-        // 이미 올렸다면 예외 처리
-
-        log.info(s3Uploader.uploadImage(imageFile));
-
-        return ResponseEntity.ok("good");
+        activityService.saveCompanionActivityHistory(uploadActivityRequestDTO, images);
+        return ResponseEntity.ok(new CommonResponseDTO(true, "활동 기록이 완료되었습니다."));
     }
-
-
 }
