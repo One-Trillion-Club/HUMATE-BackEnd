eb002c7bc6b2d9e36f3d68dcc5f26b2f8609c519
yookyung Choi <ykc131719@gmail.com>
2024-07-29 16:16:20 +0900

    ✨ Feat: MongoDBConfig로 연동

---
+++ src/main/java/com/otclub/humate/common/config/MongoDBConfig.java
@@ -0,0 +1,26 @@
+package com.otclub.humate.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoDBConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongodbUri;
+
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory() {
+        return new SimpleMongoClientDatabaseFactory(mongodbUri);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDatabaseFactory());
+    }
+}
