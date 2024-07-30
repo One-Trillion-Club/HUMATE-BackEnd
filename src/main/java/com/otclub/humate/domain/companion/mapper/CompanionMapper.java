8a97366b81c56884a6bf6a7d169b69012221bd68
Hypersand <on8214@gmail.com>
2024-07-30 09:37:02 +0900

    ✨ Feat: 후기 등록 페이지 검증 쿼리 보완

--- src/main/java/com/otclub/humate/domain/companion/mapper/CompanionMapper.java
+++ src/main/java/com/otclub/humate/domain/companion/mapper/CompanionMapper.java
@@ -1,13 +1,21 @@
 package com.otclub.humate.domain.companion.mapper;
 
+import com.otclub.humate.common.entity.Companion;
 import com.otclub.humate.domain.companion.dto.CompanionPostDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Optional;
 
 @Mapper
 public interface CompanionMapper {
     int updateCompanionStatusById(int companionId);
-    Optional<CompanionPostDTO> selectCompanionAndPost(int companionId);
 
+    Optional<CompanionPostDTO> selectCompanionAndPost(
+            @Param("companionId") int companionId,
+            @Param("memberId") String memberId);
+
+    Optional<Companion> selectCompanionByIds(
+            @Param("companionId") int companionId,
+            @Param("memberId") String memberId);
 }
