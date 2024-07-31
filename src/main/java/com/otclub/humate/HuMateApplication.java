9ce85e99a70021022ca3a474a80e622b4703265a
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-07-31 10:50:51 +0900

    Revert "[CHAT] Redis 연결"

--- src/main/java/com/otclub/humate/HuMateApplication.java
+++ src/main/java/com/otclub/humate/HuMateApplication.java
@@ -2,12 +2,10 @@ package com.otclub.humate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
-@EnableCaching
 public class HuMateApplication {
 
 	public static void main(String[] args) {
