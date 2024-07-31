bba653a607c7ae75b6034195dd8c384fec32aef2
yookyung Choi <ykc131719@gmail.com>
2024-07-31 10:39:47 +0900

    ✨ Feat: Redis 연결

--- src/main/java/com/otclub/humate/common/config/RedisConfig.java
+++ src/main/java/com/otclub/humate/common/config/RedisConfig.java
@@ -8,11 +8,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+/**
+ * Redis 연동 config
+ * @author 최유경
+ * @since 2024.07.31
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        	수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.07.31  	최유경        최초 생성
+ * </pre>
+ */
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
@@ -20,11 +31,20 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     private final RedisProperties redisProperties;
 
+    /**
+     * 단일 Topic 사용을 위한 Bean 설정
+     * @return
+     */
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
     }
 
+    /**
+     * RedisTemplate 구성하기
+     * @apiNote Redis는 default로 key-value를 String(Hash)를 저장
+     * @return RedisTemplate
+     */
     @Bean
     public RedisTemplate<?,?> redisTemplate(){ // redisTemplate default Value Type = JSON
         RedisTemplate<?,?> redisTemplate = new RedisTemplate<>();
@@ -35,14 +55,5 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
 
         return redisTemplate;
-    }
-    // Redis는 default로 key-value를 String(Hash)를 저장
-
-    @Bean
-    public RedisMessageListenerContainer redisMessageListener(){
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-
-        container.setConnectionFactory(redisConnectionFactory());
-        return container;
     }
 }
