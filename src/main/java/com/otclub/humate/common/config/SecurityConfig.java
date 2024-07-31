2ddc091750df4f1f0f39d79abb89870a3ace3cb7
yookyung Choi <ykc131719@gmail.com>
2024-07-31 12:24:34 +0900

    🔨 Fix: Redis 관련 깃 충돌 병합

--- src/main/java/com/otclub/humate/common/config/SecurityConfig.java
+++ src/main/java/com/otclub/humate/common/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
                 "/activities/**",
                 "/rooms/**",
                 "/companions/**",
-                "/reviews/**");
+                "/reviews/**",
+                "/redis/**");
     }
 }
