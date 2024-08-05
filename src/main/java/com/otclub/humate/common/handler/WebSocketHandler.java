ca76318f1a2ee6d999b41392df7409a0aa5ec1bc
yookyung Choi <ykc131719@gmail.com>
2024-08-05 10:41:08 +0900

    ✨ Feat: WebSocketHandler에서 participateId로 session 저장 및 구독, Redis관련 Service만 별도로 분리

--- src/main/java/com/otclub/humate/common/handler/WebSocketHandler.java
+++ src/main/java/com/otclub/humate/common/handler/WebSocketHandler.java
@@ -3,8 +3,8 @@ package com.otclub.humate.common.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.otclub.humate.common.service.RedisWebSocketSessionManager;
 import com.otclub.humate.domain.chat.dto.ChatMessageRequestDTO;
-import com.otclub.humate.domain.chat.service.RedisPubSubService;
-import com.otclub.humate.domain.chat.vo.MessageType;
+import com.otclub.humate.common.service.RedisPubSubService;
+import com.otclub.humate.domain.chat.service.ChatMessageService;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +19,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketHandler extends TextWebSocketHandler {
+    private final ChatMessageService chatMessageService;
     private final RedisPubSubService redisPubSubService;
     private final RedisWebSocketSessionManager sessionManager;
     private final ObjectMapper objectMapper;
@@ -26,19 +27,20 @@ public class WebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info("[afterConnectionEstablished] session : {}", session.toString());
-        log.info("[afterConnectionEstablished] session principal : {}", session.getPrincipal());
-        String memberId = extractMemberId(session);
-        log.info("[afterConnectionEstablished] memberId : {} ", memberId);
-        sessionManager.addSession(memberId, session);
+        String participateId = extractMemberId(session);
+        log.info("[afterConnectionEstablished] participateId : {} ", participateId);
+        sessionManager.addSession(participateId, session);
+        redisPubSubService.subscribe(participateId);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         log.info("[afterConnectionClosed] session : {}", session.toString());
         log.info("[afterConnectionClosed] CloseStatus {} ", status.toString());
-        String memberId = extractMemberId(session);
-        log.info("[afterConnectionClosed] memberId : {} ", memberId);
-        sessionManager.removeSession(memberId);
+        String participateId = extractMemberId(session);
+        log.info("[afterConnectionClosed] participateId : {} ", participateId);
+        sessionManager.removeSession(participateId);
+        redisPubSubService.cancelSubChannel(participateId);
     }
 
     @Override
@@ -49,14 +51,14 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
         ChatMessageRequestDTO requestDTO = objectMapper.readValue(payload, ChatMessageRequestDTO.class);
         log.info("[WebSocketHandler] - requestDTO {}" , requestDTO.toString());
-        redisPubSubService.pubSendMessageChannel(requestDTO); // 메시지를 Redis 에 publish
+
+        chatMessageService.createMessage(requestDTO); // 메시지를 Redis 에 publish
     }
 
     private String extractMemberId(WebSocketSession session){
         Map<String, List<String>> headers = session.getHandshakeHeaders();
-        List<String> authorizationHeaders = headers.get("authorization");
-        log.info("[getHandshakeHeaders] - authorization : {}",  session.getHandshakeHeaders().toString());
-        log.info("[getHandshakeHeaders] - authorization : {}",  headers.get("authorization").toString());
+        List<String> authorizationHeaders = headers.get("Authorization");
+        log.info("[getHandshakeHeaders] - authorization : {}",  headers.get("Authorization").toString());
 
         if(authorizationHeaders != null && !authorizationHeaders.isEmpty()){
             log.info("[getHandshakeHeaders] - authorization : {}", authorizationHeaders.get(0));
