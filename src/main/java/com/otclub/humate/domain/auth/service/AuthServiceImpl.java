48687e26ef16e64a34f47159e5ffeea0db95b25c
YeongWook <ehdrnr16whduddnr@naver.com>
2024-08-05 17:54:05 +0900

    ✨ Feat: 핸드폰 인증번호 생성 시 SMS 전송 api 연동 추가(cool sms)

--- src/main/java/com/otclub/humate/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/com/otclub/humate/domain/auth/service/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.otclub.humate.common.entity.Member;
 import com.otclub.humate.common.exception.CustomException;
 import com.otclub.humate.common.exception.ErrorCode;
+import com.otclub.humate.common.service.SmsService;
 import com.otclub.humate.common.upload.S3Uploader;
 import com.otclub.humate.domain.auth.dto.*;
 import com.otclub.humate.domain.auth.jwt.JwtDTO;
@@ -41,6 +42,7 @@ import static com.otclub.humate.domain.auth.util.AuthUtil.*;
  * 2024.07.30   조영욱        JWT 토큰 리프레시 추가
  * 2024.07.31   조영욱        휴대폰 인증 추가
  * 2024.07.31   조영욱        여권 인증 추가
+ * 2024.08.05   조영욱        SMS 연동 추가
  * </pre>
  */
 @Service
@@ -54,6 +56,8 @@ public class AuthServiceImpl implements AuthService {
     private final JwtGenerator jwtGenerator;
     private final StringRedisTemplate redisTemplate;
     private final S3Uploader s3Uploader;
+    private final SmsService smsService;
+
     // CODEF 액세스 토큰 발급을 위한 요청 URL
     @Value("${codef.accessTokenIssuanceUrl}")
     private String accessTokenIssuanceUrl;
@@ -200,6 +204,9 @@ public class AuthServiceImpl implements AuthService {
 
         // 레디스에 저장되는 구조: { "code:01012341234" : "012345" }
         operations.set(redisCodeKey, code, Duration.ofSeconds(REDIS_PHONE_CODE_KEY_TTL));
+
+        // SMS 메시지 전송
+        smsService.sendPhoneVerifySMS(phone, code);
 
         return code;
     }
